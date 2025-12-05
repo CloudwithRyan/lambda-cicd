5bb41c638db5feacad762a1f8fc2fa56df8fbacc
CloudwithRyan <ryan.smith.150193@gmail.com>
2025-12-04 17:45:56 -0800

    secondary testing phase

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('hello from CICD Lambda')
+        'body': json.dumps('hello from CICD Lambda testing vscode')
     }
